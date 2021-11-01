a4b0e5e4126d03add227f4ac9fcc14e089506a3a
Ekansh <ekansh.anand16@gmail.com>
2021-11-01 21:08:45 +0530

    Added forget and reset password functionality

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { register, login } from '../controllers/users.js';
+import { register, login, forgotPassword, resetPassword } from '../controllers/users.js';
 
 const router = express.Router();
 
@@ -8,5 +8,11 @@ router.post('/register', register);
 
 //To login a User
 router.post('/login', login);
+
+//To generate forgot password link
+router.post('/forgotPassword', forgotPassword);
+
+//To reset the password
+router.post('/resetPassword', resetPassword);
 
 export default router;
