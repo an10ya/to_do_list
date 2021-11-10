82a3f279f37057a0d7a2b703fb53dadcc299ecae
Ekansh <35033621+ekansh1602@users.noreply.github.com>
2021-11-11 00:29:59 +0530

    Add files via upload

---
+++ NOTES.md
@@ -0,0 +1,12 @@
+Libraries Used in the Server
+
+1. Express
+2. Cors
+3. Body-Parser
+4. Nodemon
+5. Mongoose
+6. jsonwebtoken
+7. bcryptjs
+8. Passportjs for social media authentication
+9. cookie-session
+10. passport-google-oauth20
---
+++ index.js
@@ -0,0 +1,41 @@
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cookieSession from 'cookie-session';
+import passport from 'passport';
+
+//Routes
+import userRoutes from './routes/users.js';
+
+const app = express();
+
+app.use(cors());
+app.use(cookieSession({
+    name: 'session',
+    keys: ['task'],
+    maxAge: 24*60*60*100
+}));
+app.use(passport.initialize());
+app.use(passport.session()); 
+app.use(bodyParser.json({ limit: '100mb', extended: true }));
+
+//Starting of every User route
+app.use('/users', userRoutes);
+
+app.get('/', (req,res) => {
+    //res.json({ message: 'Server is runnung '});
+    res.send('Server is running');
+});
+
+const CONNECTION_URL = `mongodb+srv://Ekansh:chelseafc@cluster0.vxtzt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const PORT = 5000;
+
+/* Async wait block 
+const dbConnection = await mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+console.log('Database connected ', dbConnection);
+*/
+
+// .then block
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => app.listen(PORT, () => console.log(`Server is running at PORT ${PORT} and connected to the database `)));
